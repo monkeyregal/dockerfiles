71a7c570b70c5daae5f59c8b0232f072c8df1452
Raymond Kroon <raymond@k3n.nl>
2016-05-08 16:37:36 +0200

    chore: build changed Dockerfiles, based on commit info
    If PUSH_IMAGE is set then pushes also

---
+++ build-changed.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+changed_files=( $(git diff-tree --no-commit-id --name-only -r -M HEAD | grep Dockerfile) )
+
+start_dir=$(pwd)
+echo $start_dir
+
+for D in "${changed_files[@]}"
+do IFS="/"; set $D
+   unset IFS
+   cd $1/$2
+
+   echo "Building $1 $2"
+   docker build --rm -t monkeyregal/$1:$2 .
+
+   if [ -n "$PUSH_IMAGE" ]; then
+       echo "PUSHING"
+       docker push monkeyregal/$1:$2
+   fi
+
+   cd $start_dir
+done
