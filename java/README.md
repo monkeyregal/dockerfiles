2d8765a467e8f002d3a6ca5fa6d2cfcee89f12b5
Raymond Kroon <raymond.kroon@kadaster.nl>
2016-05-09 11:17:14 +0200

    refac: use main jdk versions
    Update checks the latest docker base for the java version, and updates the dockerfile (or VERSION actually)

--- java/README.md
+++ java/README.md
@@ -1,6 +1,6 @@
 How to use
 ======
 
-Make an alias like `alias java="docker run --rm -ti -v $PWD:/home/user/app -v --net host monkeyregal/java:openjdk-8u77-alpine"`
+Make an alias like `alias java="docker run --rm -ti -v $PWD:/home/user/app -v --net host monkeyregal/java:openjdk-8-alpine"`
 
 Preferably take the alpine versions, since they are smaller. If you cannot use Alpine linux (they have a different libc), then use the not Alpine version.
---
+++ java/update.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+versions=(
+    openjdk-6
+    openjdk-7
+    openjdk-7-alpine
+    openjdk-8
+    openjdk-8-alpine
+)
+
+for v in "${versions[@]}"
+do
+   targetdir=$v
+
+   mkdir -p $targetdir
+
+   current_version=
+   [[ -f $targetdir/VERSION ]] && current_version=$(cat $targetdir/VERSION)
+   docker pull java:$v
+   export $(docker inspect --type image --format='{{range .Config.Env }}{{println .}}{{end}}'  java:$v | grep "^JAVA_VERSION")
+
+   # Only generate when base image version is updated
+   if [[ $current_version != $JAVA_VERSION ]]; then
+
+       echo "Updating $v to $JAVA_VERSION"
+
+       printf "$JAVA_VERSION" > $targetdir/VERSION
+
+       generate_script=generate-dockerfile.sh
+       if [[ $v == *"alpine"* ]]; then
+           generate_script=generate-dockerfile-alpine.sh
+       fi
+
+       JAVA_BASE=$v ./$generate_script > $targetdir/Dockerfile
+   fi
+done
