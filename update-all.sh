0799771d5526e6bce1d8e90f3db4f3a10f36816c
Raymond Kroon <raymond.kroon@kadaster.nl>
2016-05-09 15:39:08 +0200

    fix: update-all with maven

--- update-all.sh
+++ update-all.sh
@@ -5,3 +5,4 @@ set -e
 echo "Update Java" && java/update.sh
 echo "Update Leiningen" && clojure/update-leiningen.sh
 echo "Update Boot" && clojure/update-boot.sh
+echo "Update Maven" && maven/update.sh
